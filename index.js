33fa55febfac0eae23d1fd152527dc69d57d0702
andrebmachadoo <andrebmachadoo@gmail.com>
2023-12-29 01:09:44 -0300

    Insert version param

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var pathTranspiler = process.argv[0]
 var pathThisFile = process.argv[1]
 params.splice(0, 2);
 
-var validParams     = [ 'name', 'f','help','path']
+var validParams     = [ 'name', 'f','help','path','v']
 var sanitizedParams = {}
 var tempName = ''
 params.forEach(v =>{
@@ -22,7 +22,9 @@ if(sanitizedParams.name) sanitizedParams.name = sanitizedParams.name.join('')
 if(sanitizedParams.f) sanitizedParams.f = sanitizedParams.f.join(',')
 
 // Events
-if(sanitizedParams.h){
+if(validParams.includes('v')){
+  console.log('JavaScript Console interface version 1.0.8')
+}else if(sanitizedParams.h){
   console.log(help,'\n', pathTranspiler,pathThisFile)
 }else{
   const replace = new ReplaceValues({
